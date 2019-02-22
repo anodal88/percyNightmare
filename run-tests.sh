8bb2fbe585ae5cd15ef6403601fa5098612b923e
anodal <anodal@ncl.com>
2019-02-22 13:08:22 -0500

    clean up the sh file

--- run-tests.sh
+++ run-tests.sh
@@ -3,12 +3,6 @@
 set -o pipefail
 set -e
 
-# Run a background HTTP server to serve our JavaScript and test against.
-##./node_modules/.bin/http-server . -p 8000 --silent &
-##TEST_SERVER_PID=$!
-
 # Run all our tests.
 ./node_modules/.bin/mocha --recursive
 
-# Kill our backgrounded server (http-server expects a Ctrl+C, aka SIGINT)
-##kill -SIGINT $TEST_SERVER_PID
